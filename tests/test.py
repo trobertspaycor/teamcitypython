4255bce0cf590c12d072b2c81fcecde338ec809e
trobertspaycor <troberts@paycor.com>
2024-07-17 14:59:10 -0500

    fixed spacing errors

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
 import unittest
 from ..main import main
 
+
 class TestMainFunction(unittest.TestCase):
     def test_main(self):
         self.assertEqual(main(3), 9)
+
 
 if __name__ == "__main__":
     unittest.main()
